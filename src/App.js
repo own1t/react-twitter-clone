69c4e792e4b2ae4d5e9ac417216a04d1032cef9f
Ryan Kim <iawal86@gmail.com>
2020-09-11 18:15:23 -0700

    created sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 
+import Sidebar from "./components/Sidebar";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <Sidebar />
+    </div>
+  );
 }
 
 export default App;
